daf7176e1190d33ee995f8346b2667594b51905b
NeharikaR <76840829+NeharikaR@users.noreply.github.com>
2021-01-29 17:01:43 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,27 @@
+class Ground
+{
+    constructor(x,y,width,height)
+    {
+        var options ={
+            isStatic:true         
+           // friction:0.3,
+           // density :1.0
+        }
+    
+        this.body = Bodies.rectangle(x,y,width,height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world,this.body);
+
+    }
+
+    display()
+    {
+
+        rectMode(CENTER);
+        fill("cyan");
+        rect(this.body.position.x, this.body.position.y, this.width,this.height );
+
+    }
+
+}
